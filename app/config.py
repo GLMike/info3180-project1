74e2844f07e349cdbd5da8ca790dedfc4bdc2463
Shane-Michael Thompson <shane2michael@gmail.com>
2021-03-23 22:58:51 -0500

    ye

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     UPLOAD_FOLDER = './uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:123456@localhost/project1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://proj1:proj1@localhost/info3180project1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     
     if os.path.isdir(UPLOAD_FOLDER) is False:
